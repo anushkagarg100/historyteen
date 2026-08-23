4325687a73b5096a11b8d1864893ba86cdd4afa8
Anushka Garg <anushkagargrules@gmail.com>
2026-08-23 12:50:20 -0700

    Add older reader book recommendations

--- app/book-recommendations/page.tsx
+++ app/book-recommendations/page.tsx
@@ -10,15 +10,22 @@ import childsIntroductionPoetryCover from "../../public/childs-introduction-poet
 import foodFightCover from "../../public/food-fight-cover.png";
 import geronimoStiltonCover from "../../public/geronimo-stilton-journey-through-time.png";
 import goodnightStoriesCover from "../../public/goodnight-stories-rebel-girls-cover.png";
+import gunsGermsSteelCover from "../../public/guns-germs-steel-cover.png";
 import herstoryCover from "../../public/herstory-cover.png";
 import heroesOfOlympusCover from "../../public/heroes-of-olympus-series.png";
+import howWeGotToNowCover from "../../public/how-we-got-to-now-cover.png";
+import indigenousPeoplesHistoryCover from "../../public/indigenous-peoples-history-cover.png";
 import kidActivistsCover from "../../public/kid-activists.png";
 import kidArtistsCover from "../../public/kid-artists.png";
 import littleMonstersCover from "../../public/little-monsters-cover.png";
+import liesMyTeacherCover from "../../public/lies-my-teacher-told-me-cover.png";
 import longWalkToWaterCover from "../../public/long-walk-to-water-cover.png";
 import meetGreatComposersCover from "../../public/meet-great-composers.png";
 import pabloPicassoCover from "../../public/who-was-pablo-picasso.png";
 import percyJacksonCover from "../../public/percy-jackson-series.png";
+import poisonSquadCover from "../../public/poison-squad-cover.png";
+import sapiensCover from "../../public/sapiens-cover.png";
+import shortestHistoryChinaCover from "../../public/shortest-history-china-cover.png";
 import trialsOfApolloCover from "../../public/trials-of-apollo-series.png";
 import travelBookCover from "../../public/travel-book-cover.png";
 import storiesGreatComposersCover from "../../public/stories-great-composers.png";
@@ -521,6 +528,314 @@ export default function BookRecommendationsPage() {
                   className={styles.bookSideCover}
                   src={foodFightCover}
                   alt="Cover of Food Fight!"
+                  sizes="(max-width: 720px) 70vw, 230px"
+                />
+              </div>
+            </article>
+          </section>
+
+          <section className={styles.recommendationSection}>
+            <h2>For Older Readers:</h2>
+            <article className={styles.bookRecommendation}>
+              <h3>Sapiens</h3>
+              <p className={styles.bookAuthor}>By Yuval Noah Harari</p>
+              <div className={styles.bookWithCover}>
+                <div className={styles.bookReview}>
+                <p>
+                  Sapiens. A book full of contradictions. Readable yet too long.
+                  Interesting, yet also boring. Razor sharp at times, but at
+                  other times subscribing to the same banal narrative.
+                </p>
+                <p>
+                  Truth be told, I don&rsquo;t have a straight recommendation
+                  for you. The Chronicle of Higher Education said of this book:
+                  &ldquo;Reading Sapiens is like joining a wide-ranging and
+                  provocative dinner-table conversation. You may not agree with
+                  it all, but it makes for a memorable meal.&rdquo; I have a
+                  love-hate relationship with the book, but to judge it fairly,
+                  we&rsquo;ll pretend it&rsquo;s a dinner-party conversation, with
+                  Harari as the intelligent guest who loves giving their opinion
+                  (Every party has one of those).
+                </p>
+                <p>
+                  First, I love Sapiens because of its readability. Too many
+                  history books are boring, dense, and overstuffed with
+                  information, but that wasn&rsquo;t an issue with Sapiens.
+                  Moving back to the comparison, the book reminded me of a
+                  conversation—intelligent, but never too hard for the reader to
+                  understand.
+                </p>
+                <p>
+                  Not only that, but like any good discussion, it had some
+                  particularly insightful pockets. For example, Harari discusses
+                  the US Constitution in the book. The Constitution states that
+                  &ldquo;All men are created equal.&rdquo; The concept of
+                  &ldquo;all men being equal&rdquo; had been drilled into me at
+                  school, and, like all the kids in my class, I fervently
+                  believed it. But, Harari challenges, were all men created
+                  equal? Were all men created unequal (as ancient Babylonian
+                  society would have you believe)? No, says Harari. These were
+                  just creation myths to hold a particular society together,
+                  depending on how they chose to structure themselves. If you
+                  think about it, humans were just created. The concept of
+                  &ldquo;equality&rdquo; was dreamed up by our leaders.
+                </p>
+                <p>
+                  &ldquo;All men are created equal&rdquo; is propaganda! I
+                  remember closing the book, because I couldn&rsquo;t believe
+                  what I was reading. These are the best parts of any
+                  conversation. Somebody says something that makes us
+                  uncomfortable—but also makes us think.
+                </p>
+                <p>
+                  But just because you love giving your discourses at a dinner
+                  party doesn&rsquo;t mean all of them are necessarily correct or
+                  make sense. Herein lies Sapiens&rsquo;s problem. A lot of the
+                  book is Harari sounding less like an intellectual and more
+                  like a disgruntled guest. It&rsquo;s not anything new, though.
+                  Sapiens, in fact forms a predictable narrative.
+                </p>
+                <p>
+                  Section one: We were happy hunter-gatherers, who, after
+                  hunting and gathering, hung out with each other near the fire.
+                  Life was great! Our bodies were perfectly adapted to this
+                  lifestyle, and we could have gone on living like this forever.
+                  What went wrong?
+                </p>
+                <p>
+                  Section two: Whoops! What a bummer. We accidentally handed
+                  away our freedom to agriculture. Now we are forced to grow
+                  wheat in the blazing sun instead of frolicking through the
+                  forest like we used to. Not only that, but we can&rsquo;t even
+                  enjoy the fruits of our labor. We have to hand them over to an
+                  evil emperor. Oh, the misery!
+                </p>
+                <p>
+                  Section three: The Industrial Revolution rolls around, and
+                  with it, the state and the market (who Harari calls &ldquo;the
+                  mother and father of the individual&rdquo;) Enter the modern
+                  age, when the state and the market are great and we are all
+                  indulgent consumers.
+                </p>
+                <p>
+                  As a fifth-grader, I thought this was groundbreaking. But
+                  after reading more, I now realize that this is an
+                  oversimplification. First of all, (as I will discuss in the
+                  Dawn of Everything recommendation) humans didn&rsquo;t
+                  mechanically switch from being idyllic hunter-gatherers to
+                  miserable farmers. The reality, depending on region to region,
+                  is far more complex, with many different types of foraging
+                  societies. I&rsquo;m not saying that Harari&rsquo;s narrative
+                  isn&rsquo;t true. Some part of it may well be. However, it seems
+                  to be more nuanced than what he suggests. Furthermore, towards
+                  the end of the book, Harari seems to be blaming the individual
+                  and sneaking in praises of capitalism and the government
+                  wherever he can.
+                </p>
+                <p>
+                  Of course, this isn&rsquo;t the entirety of the book. Sapiens
+                  has a number of ideas—about feminism, race, money, science,
+                  and a whole host of things. It&rsquo;s definitely worth reading.
+                  But at its core, Sapiens is a jumble of interesting
+                  dinner-party ideas, some of which should be taken with a pinch
+                  (or perhaps a handful) of salt.
+                </p>
+                </div>
+                <Image
+                  className={styles.bookSideCover}
+                  src={sapiensCover}
+                  alt="Cover of Sapiens"
+                  sizes="(max-width: 720px) 70vw, 230px"
+                />
+              </div>
+            </article>
+
+            <article className={styles.bookRecommendation}>
+              <h3>Guns, Germs, and Steel</h3>
+              <p className={styles.bookAuthor}>By Jared Diamond</p>
+              <div className={styles.bookWithCover}>
+                <div className={styles.bookReview}>
+                <p>
+                  So, this book is essentially Sapiens&rsquo;s grandfather (Harari
+                  himself credits Guns, Germs, and Steel for giving him the idea
+                  to write it) with the same Eurocentric approach to human
+                  history. Even though I haven&rsquo;t read it, I do know there
+                  are some crucial differences. It won a Pulitzer Prize and is
+                  far more detailed than Sapiens.
+                </p>
+                <p>
+                  I believe that both Sapiens and Guns, Germs, and Steel are good
+                  bases to start your history journey. But readers should also
+                  compare and challenge the ideas presented in these
+                  books—indeed, comparison is one of the best parts of history.
+                </p>
+                </div>
+                <Image
+                  className={styles.bookSideCover}
+                  src={gunsGermsSteelCover}
+                  alt="Cover of Guns, Germs, and Steel"
+                  sizes="(max-width: 720px) 70vw, 230px"
+                />
+              </div>
+            </article>
+
+            <article className={styles.bookRecommendation}>
+              <h3>The Shortest History series</h3>
+              <p className={styles.bookAuthor}>By multiple authors</p>
+              <div className={styles.bookWithCover}>
+                <div className={styles.bookReview}>
+                <p>
+                  Finally, we are back to &ldquo;regular history&rdquo; so to
+                  speak. If you want to get an overview of the past of different
+                  countries (China, India, the US) this is an informative way to
+                  do so. What I also like about this series is that it keeps
+                  history relevant. I have read The Shortest History of China,
+                  and the author frequently places China&rsquo;s history in
+                  context of where it is today (as an emerging global
+                  superpower). Not only that, but some of the books talk about
+                  pertinent topics today, such as AI and Israel and Palestine.
+                </p>
+                </div>
+                <figure className={styles.bookSideFigure}>
+                  <Image
+                    className={styles.bookSideCover}
+                    src={shortestHistoryChinaCover}
+                    alt="Cover of The Shortest History of China"
+                    sizes="(max-width: 720px) 70vw, 230px"
+                  />
+                  <figcaption>My Favorite</figcaption>
+                </figure>
+              </div>
+            </article>
+
+            <article className={styles.bookRecommendation}>
+              <h3>Lies My Teacher Told Me</h3>
+              <p className={styles.bookAuthor}>By James Loewen</p>
+              <div className={styles.bookWithCover}>
+                <div className={styles.bookReview}>
+                <p>
+                  With Florida declaring new standards for learning Black
+                  history (such as &ldquo;slavery might have been good for
+                  slaves&rdquo;) and Trump&rsquo;s Americana military pageants,
+                  this book deserves a reread.
+                </p>
+                <p>
+                  Lies My Teacher Told Me is essentially an
+                  &ldquo;anti-textbook&rdquo; that provides a fascinating view of
+                  US history, one that we are not often taught in school. Before
+                  reading this, I had heard about Helen Keller about a thousand
+                  times in school, yet I had no idea she was actually a
+                  socialist. I was taught about the Plymouth colony in my US
+                  history class, but I did not learn that European diseases
+                  wiped out 98 percent of the native population.
+                </p>
+                <p>
+                  With insightful teachings (or re-teachings, if you are in
+                  middle and high school) about Indigenous people, the Vietnam
+                  War, recent history, the US government, and more, you should
+                  read this book to truly understand America beyond sanitized
+                  media portrayals and what you are told in school.
+                </p>
+                <p>
+                  One last thing: Since Loewen first wrote this book in 1995, US
+                  history education has definitely improved, particularly in
+                  terms of racial and gender diversity in lessons. Since the
+                  1995 and 2007 ones are a little outdated, so I would suggest
+                  reading the 2018 edition.
+                </p>
+                </div>
+                <Image
+                  className={styles.bookSideCover}
+                  src={liesMyTeacherCover}
+                  alt="Cover of Lies My Teacher Told Me"
+                  sizes="(max-width: 720px) 70vw, 230px"
+                />
+              </div>
+            </article>
+
+            <article className={styles.bookRecommendation}>
+              <h3>How We Got To Now</h3>
+              <p className={styles.bookAuthor}>By Steven Johnson</p>
+              <div className={styles.bookWithCover}>
+                <div className={styles.bookReview}>
+                <p>
+                  You may have heard of the &ldquo;butterfly effect&rdquo; where a
+                  butterfly flaps its wings and a hurricane starts a thousand
+                  miles away. But this book discusses the &ldquo;hummingbird
+                  effect&rdquo; where when a particular thing develops, a
+                  seemingly unrelated thing develops with it—and uses this logic
+                  to discuss some of humanity&rsquo;s greatest inventions (for
+                  example, glasses and the printing press were created at the
+                  same time because reading made people realize they were
+                  farsighted).
+                </p>
+                <p>
+                  I am using this book as one of the guides to help me research
+                  for NHD (because this year&rsquo;s theme is Innovation in
+                  History) and I am excited to see where it takes me.
+                </p>
+                </div>
+                <Image
+                  className={styles.bookSideCover}
+                  src={howWeGotToNowCover}
+                  alt="Cover of How We Got To Now"
+                  sizes="(max-width: 720px) 70vw, 230px"
+                />
+              </div>
+            </article>
+
+            <article className={styles.bookRecommendation}>
+              <h3>The Poison Squad</h3>
+              <p className={styles.bookAuthor}>By Deborah Blum</p>
+              <div className={styles.bookWithCover}>
+                <div className={styles.bookReview}>
+                <p>Caution: This book is not for the faint of stomach.</p>
+                <p>
+                  Honey used to be made of corn syrup, milk had formaldehyde
+                  (chemical used to preserve dead bodies) in it, and strawberry
+                  jam, despite the name, had no strawberries. This was the
+                  reality of food in the late 19th century—that is, until
+                  chemist Harvey Washington Wiley stepped in. Wiley was the
+                  chief chemist at the USDA, and The Poison Squad follows his
+                  journey as he exposed food adulteration and eventually created
+                  the FDA. The story touches on interesting, yet little-known
+                  historical events, such as the &ldquo;embalmed beef&rdquo;
+                  Spanish-American War scandal. In fact, I used it as a research
+                  source in my NHD project (For more about my projects, click on
+                  My NHD Experience).
+                </p>
+                <p>
+                  The book is kind of unconventional and not what you would
+                  think of as a &ldquo;history book&rdquo; but definitely one I
+                  would recommend. Just a warning; you may want to keep a bag
+                  ready!
+                </p>
+                </div>
+                <Image
+                  className={styles.bookSideCover}
+                  src={poisonSquadCover}
+                  alt="Cover of The Poison Squad"
+                  sizes="(max-width: 720px) 70vw, 230px"
+                />
+              </div>
+            </article>
+
+            <article className={styles.bookRecommendation}>
+              <h3>An Indigenous People&rsquo;s History of the United States</h3>
+              <p className={styles.bookAuthor}>By Roxanne Dunbar-Ortiz</p>
+              <div className={styles.bookWithCover}>
+                <div className={styles.bookReview}>
+                <p>
+                  Very similar to Lies My Teacher Told Me, except it&rsquo;s for
+                  older readers and has won numerous awards (such as the
+                  American Book Award). I have just begun reading it, but it
+                  seems really solid and presents a host of sobering facts.
+                </p>
+                </div>
+                <Image
+                  className={styles.bookSideCover}
+                  src={indigenousPeoplesHistoryCover}
+                  alt="Cover of An Indigenous Peoples' History of the United States"
                   sizes="(max-width: 720px) 70vw, 230px"
                 />
               </div>
