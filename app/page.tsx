cd1fcadcdbf87de490b8e89759b26b585e81de0a
Anushka Garg <anushkagargrules@gmail.com>
2026-09-13 11:33:09 -0700

    Punctuate homepage headline

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
                 </figure>
               </div>
               <div className={styles.heroHeadline}>
-                <h1>YOUR history space</h1>
+                <h1>YOUR history space.</h1>
                 <p className={styles.heroSubhead}>Ready to dive in?</p>
               </div>
             </div>
